cd20fcade8f6914fb22e1f1bca0016a5e94a9abb
gaffey <253896514@qq.com>
2023-09-11 15:07:37 +0800

    Create Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+Set-Alias vi nvim
+Set-Alias vim nvim
+function cdtest {
+    Set-Location -Path "D:\workspace\fc\test_client\client\"
+}
+function cdserver {
+    Set-Location -Path "D:\workspace\fc\server-beta"
+}
